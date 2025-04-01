f4403f4aa4fde0b900e8fb7d4cc68b54111a1805
Vijay <vijayk299860@gmail.com>
2025-04-02 01:33:25 +0530

    domain updates

--- taskname/settings.py
+++ taskname/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["localhost","127.0.0.1", "web-production-taskmate-vk.up.railway.app"]
+CSRF_TRUSTED_ORIGINS = ['web-production-taskmate-vk.up.railway.app']
 
 
 # Application definition
